7df3f1f7e9f259423baaa95544414158179b6226
malfraip <pierrick@pierrick-XPS-16.(none)>
2014-04-15 01:01:45 +0200

    adding python script use in second galileo architecture to read data from the sensor

---
+++ Python/gazValue.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import time
+import os
+
+pGazValue='0'
+gazValue='0'
+
+while True:
+	f = open ("/sys/bus/iio/devices/iio:device0/in_voltage0_raw","r")
+	gazValue = f.readline()
+	if (gazValue != pGazValue):
+		print "new gazValue : " + gazValue
+		pGazValue = gazValue
+                
+		os.system("mosquitto_pub -h 10.42.0.1 -t gazValue -m " + gazValue)
+		
+	f.close()
+	time.sleep(1);
